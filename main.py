efdec6f580ff96c0f656719d9069ed44ef024c72
Mohammad <Ehteshami1378@gmail.com>
2023-01-13 20:49:55 +0100

    add trim_audios

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def read_data(csv_path="./dsl_data/development.csv"):
     Returns:
         pandas dataFrame: the x has all the features, excluding "path", "action", "object" and "Id".
         the y has "action", "object", and "intention" which is a new feature by adding the other two columns.
+        int: sample rate
     """
 
     development_pd = pd.read_csv(csv_path)  # .drop(["Id"], axis=1)
@@ -25,7 +26,28 @@ def read_data(csv_path="./dsl_data/development.csv"):
     y = development_pd[["action", "object"]]
     y["intention"] = development_pd["action"] + development_pd["object"]
 
-    return x, y
+    return x, y, srr
 
 
-X, Y = read_data()
+def trim_audios(x, top_db=30, hop_length=50):
+    """Trim the audios in the X.
+
+    Args:
+        x (pandas dataFrame): DataFrame consist of features, incuding the audios
+        top_db (int, optional): audio's db less than this number will consider as silent. Defaults to 30.
+        hop_length (int, optional): sensitivity of triming. Defaults to 50.
+
+    Returns:
+        pandas dataFrame: features DataFrame, with trimed audio in the "Signal" column.
+    """
+    for index, row in x.iterrows():
+        wave, i = librosa.effects.trim(
+            row["Signal"], top_db=top_db, hop_length=hop_length
+        )
+        x.at[index, "Signal"] = wave
+
+    return x
+
+
+X, Y, srr = read_data()
+X_trimed = trim_audios(X.copy())
